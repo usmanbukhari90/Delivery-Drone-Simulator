c6f37ec23e43e191f6a691a17ff5b4c24ee3a997
usmanbukhari90 <mrbukhari900@gmail.com>
2025-10-20 01:22:32 +0500

    Working started

--- deliverydronesimulator.cpp
+++ deliverydronesimulator.cpp
@@ -1,1 +1,12 @@
-
+#include<iostream>
+#include<conio.h>
+#include<algorithm>
+#include<iomanip>
+#include<math.h>
+using namespace std;
+int main()
+{
+cout<<"Drone Delivery Simulator!";
+_getch();
+return 0;
+}
